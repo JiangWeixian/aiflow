2c0ba58518b3e2f8b21cce7b6771751b79d34a45
JiangWeixian <jiangweixian1994@gmail.com>
2023-03-29 21:47:01 +0800

    :dizzy:  wh

--- src/contentScripts/components/chat/index.tsx
+++ src/contentScripts/components/chat/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { a, useSpring } from '@react-spring/web'
+import { a, config, useSpring } from '@react-spring/web'
 import clsx from 'clsx'
 
 import type { ChatMessage } from '~/logic/openai/types'
@@ -26,7 +26,12 @@ export const Chat = () => {
   const { conventions } = useBearStore()
   const [toggle, setToggle] = useState(false)
   const [activeConventionId, setActiveConventionId] = useState<string | null>(null)
-  const props = useSpring({ h: toggle ? 600 : 0, opacity: toggle ? 1 : 0 })
+  const props = useSpring({
+    h: toggle ? 600 : 0,
+    w: toggle ? 500 : 100,
+    opacity: toggle ? 1 : 0,
+    config: config.default,
+  })
 
   // const sendMessage = () => {
   //   if (input) {
@@ -46,32 +51,38 @@ export const Chat = () => {
   const messages: Partial<ChatMessage>[] = !controlledActiveConventionId ? [] : conventions[controlledActiveConventionId]
 
   return (
-    <div className="aiflow-chat z-100 leading-1em fixed right-10  bottom-0 m-5 flex flex-col overflow-hidden rounded-lg border border-mayumi-gray-700 bg-mayumi-gray-200 font-sans shadow-lg">
+    <div className="aiflow-chat z-100 leading-1em fixed right-0 bottom-0 m-5 flex max-w-[500px] flex-col overflow-hidden rounded-lg border border-mayumi-gray-700 bg-mayumi-gray-200 font-sans shadow-lg">
       <div
         onClick={() => {
           setToggle(prev => !prev)
         }}
         className={
           clsx(
-            'flex cursor-pointer select-none items-center justify-between border-mayumi-gray-700 p-3 text-mayumi-gray-1100 hover:bg-mayumi-gray-400',
+            'flex-0 flex cursor-pointer select-none items-center justify-between border-mayumi-gray-700 p-3 text-mayumi-gray-1100 hover:bg-mayumi-gray-400',
             {
               'border-b-0': !toggle,
               'border-b': toggle,
             },
           )}
         >
-        <h2 className="rounded-md bg-mayumi-gray-700 px-3 py-1 text-xs uppercase shadow">
+        <h2 className="rounded-md bg-transparent px-3 py-1 text-sm font-bold uppercase shadow">
           Chat
         </h2>
-        { toggle ? <i className="gg-math-minus/0.8 text-mayumi-gray-700" /> : <i className="gg-math-plus/0.8 text-mayumi-gray-700" /> }
       </div>
-      <a.div style={{ height: props.h, opacity: props.opacity }} className="flex">
+      <a.div style={{ height: props.h, opacity: props.opacity, width: props.w }} className="flex">
         {/* conventions chats list */}
         <div className="grid grid-flow-row auto-rows-max border-r border-mayumi-gray-700 p-2 font-semibold">
           {conventionIds.map((id) => {
             return (
               <div
-                className="flex items-center rounded-md bg-mayumi-gray-200 py-2 px-3 text-sm text-mayumi-gray-1100 hover:bg-mayumi-blue-400 hover:text-mayumi-gray-1200"
+                className={
+                  clsx(
+                    'flex items-center rounded-md bg-mayumi-gray-200 py-2 px-3 text-sm text-mayumi-gray-1100 hover:bg-mayumi-blue-400 hover:text-mayumi-gray-1200',
+                    {
+                      'bg-mayumi-blue-400 text-mayumi-gray-1200': id === controlledActiveConventionId,
+                    },
+                  )
+                }
                 key={id}
                 onClick={() => {
                   setActiveConventionId(id)
@@ -82,7 +93,7 @@ export const Chat = () => {
             )
           })}
         </div>
-        <div className="aiflow-messages w-[400px] max-w-[400px] bg-mayumi-gray-300">
+        <div className="aiflow-messages flex-1 bg-mayumi-gray-300">
           {messages.map((message, index) => (
             <MessageItem key={index} message={message} />
           ))}
