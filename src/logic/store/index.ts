d175eedb2b276d259f0d1a382bb111c214371f85
JiangWeixian <jiangweixian1994@gmail.com>
2023-04-11 23:09:07 +0800

    :heavy_plus_sign:  zustand-middlewares

--- src/logic/store/index.ts
+++ src/logic/store/index.ts
@@ -1,9 +1,8 @@
 import { create } from 'zustand'
-import type { StateStorage } from 'zustand/middleware'
 import { createJSONStorage, persist } from 'zustand/middleware'
 import { compact } from 'lodash-es'
-import browsers from 'webextension-polyfill'
-import type { StateCreator, StoreApi, StoreMutatorIdentifier } from 'zustand'
+import { local } from '@jiangweixian1994/zustand-middlwares/storage'
+import { logger } from '@jiangweixian1994/zustand-middlwares/devtools'
 
 import type { ChatMessage } from '~/logic/openai/types'
 import { getConvention } from '~/logic/conventions'
@@ -18,105 +17,6 @@ interface BearState {
   upsertConventions: (conventionId: string, msg: ChatMessage) => void
   clear: () => void
 }
-
-const browserLocalStorage: StateStorage = {
-  getItem: async (name: string) => {
-    const result = await browsers.storage.local.get(name)
-    return result[name]
-  },
-  setItem(name, value) {
-    return browsers.storage.local.set({ [name]: value })
-  },
-  removeItem(name) {
-    return browsers.storage.local.remove(name)
-  },
-}
-
-type Logger = <
-  T,
-  Mps extends [StoreMutatorIdentifier, unknown][] = [],
-  Mcs extends [StoreMutatorIdentifier, unknown][] = [],
->(
-  f: StateCreator<T, [...Mps, ['logger', never]], Mcs>,
-  options: {
-    name?: string
-  }
-) => StateCreator<T, Mps, [['logger', never], ...Mcs]>
-
-type LoggerImpl = <T>(
-  f: StateCreator<T, [], []>,
-  options: {
-    name?: string
-  }
-) => StateCreator<T, [], []>
-
-type Cast<T, U> = T extends U ? T : U
-type Write<T, U> = Omit<T, keyof U> & U
-type TakeTwo<T> = T extends { length: 0 }
-  ? [undefined, undefined]
-  : T extends { length: 1 }
-    ? [...a0: Cast<T, unknown[]>, a1: undefined]
-    : T extends { length: 0 | 1 }
-      ? [...a0: Cast<T, unknown[]>, a1: undefined]
-      : T extends { length: 2 }
-        ? T
-        : T extends { length: 1 | 2 }
-          ? T
-          : T extends { length: 0 | 1 | 2 }
-            ? T
-            : T extends [infer A0, infer A1, ...unknown[]]
-              ? [A0, A1]
-              : T extends [infer A0, (infer A1)?, ...unknown[]]
-                ? [A0, A1?]
-                : T extends [(infer A0)?, (infer A1)?, ...unknown[]]
-                  ? [A0?, A1?]
-                  : never
-
-type WithDevtools<S> = Write<S, StoreDevtools<S>>
-
-type StoreDevtools<S> = S extends {
-  setState: (...a: infer Sa) => infer Sr
-}
-  ? {
-      setState<A extends string | { type: unknown }>(
-        ...a: [...a: TakeTwo<Sa>, action?: A]
-      ): Sr
-    }
-  : never
-export type NamedSet<T> = WithDevtools<StoreApi<T>>['setState']
-
-declare module 'zustand' {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  interface StoreMutators<S, A> {
-    'logger': WithDevtools<S>
-  }
-}
-interface Action<T = any> {
-  type: T
-}
-
-const loggerImpl: LoggerImpl = (f, options) => (set, get, store) => {
-  type T = ReturnType<typeof f>
-  const loggedSet: NamedSet<T> = (state, replace, nameOrAction) => {
-    const action: Action<unknown>
-      = nameOrAction === undefined
-        ? { type: 'anonymous' }
-        : typeof nameOrAction === 'string'
-          ? { type: nameOrAction }
-          : nameOrAction
-    console.group(`%c action: ${options.name} : ${action.type}`, 'font-weight: bold;')
-    console.log('%c prev state:', 'color: gray; font-weight: bold;', get())
-    console.log('%c action:', 'color: #0066db; font-weight: bold;', action)
-    set(state, replace)
-    console.log('%cnext state:', 'color: #006b3b; font-weight: bold;', get())
-    console.groupEnd()
-  }
-  store.setState = loggedSet
-
-  return f(loggedSet, get, store)
-}
-
-export const logger = loggerImpl as unknown as Logger
 
 export const useBearStore = create<BearState>()(
   logger(
@@ -149,7 +49,7 @@ export const useBearStore = create<BearState>()(
       }),
       {
         name: 'aiflow-message-store',
-        storage: createJSONStorage(() => browserLocalStorage),
+        storage: createJSONStorage(() => local),
       },
     ),
     {
